88392475170a7a255be9baec6d560e815e4c2286
ktonosaki <67316927+ktonosaki@users.noreply.github.com>
2024-06-20 15:56:39 +0900

    change2

--- 01.functions.R
+++ 01.functions.R
@@ -1,15 +1,34 @@
 library("edgeR")
-library("ggplot2")
-library("ggsci")
-library("ggplot2")
-library("TCC")
-library(Biobase)
-library(dplyr)
-library('plotrix')
-library('reshape2')
 
+modidyData.mat <- function(data, depth, seed){
+  names(data) <- head
+  data1.ratio <- merge(data, seed[,c(1,4)], by = "locus_name", all =T)
+  data1.ratio$ratio[is.na(data1.ratio$ratio)] <- 1
+  data1.ratio <- na.omit(data1.ratio)
 
-modidyData <- function(data, depth){
+  row.names(data1.ratio) <- data1.ratio$locus_name
+  data1.ratio$locus_name <- NULL
+  data1.ratio$class <- NULL
+  filter <- data.frame(row.names = row.names(data1.ratio))
+  filter$Nefref_1 <- rowSums(data1.ratio[, grepl("Nefref_1", colnames(data1.ratio))]) >= depth
+  filter$Refnef_1 <- rowSums(data1.ratio[, grepl("Refnef_1", colnames(data1.ratio))]) >= depth
+  filter$Nefref_2 <- rowSums(data1.ratio[, grepl("Nefref_2", colnames(data1.ratio))]) >= depth
+  filter$Refnef_2 <- rowSums(data1.ratio[, grepl("Refnef_2", colnames(data1.ratio))]) >= depth
+  data1.ratio$filter <- apply(filter, 1, any)
+  data.rm <- filter(data1.ratio, filter == TRUE)[, -ncol(data1.ratio)]
+
+  # matrate apply
+  data.rm2 <- data.rm
+  data.rm2$mat_Refnef_1 <- data.rm$mat_Refnef_1 * data.rm$ratio
+  data.rm2$mat_Refnef_2 <- data.rm$mat_Refnef_2 * data.rm$ratio
+  data.rm2$mat_Nefref_1 <- data.rm$mat_Nefref_1 * data.rm$ratio
+  data.rm2$mat_Nefref_2 <- data.rm$mat_Nefref_2 * data.rm$ratio
+  mod_data1 <- data.rm2[, 1:8]
+
+  return(mod_data1)
+}
+
+modidyData.pat <- function(data, depth){
   print(dim(data))
   names(data) <- head
   row.names(data) <- data$locus_name
@@ -26,40 +45,7 @@ modidyData <- function(data, depth){
   return(data.rm)
 }
 
-modidyData.SCfilt <- function(data, depth, seed){
-  names(data) <- head
-  data1.ratio <- merge(data, seed[,c(1,4)], by = "locus_name", all =T)
-  data1.ratio$ratio[is.na(data1.ratio$ratio)] <- 1
-  data1.ratio <- na.omit(data1.ratio)
-  #  data1.ratio <- subset(data1.ratio, ratio > 0 )
-
-  print(dim(data1.ratio))
-  row.names(data1.ratio) <- data1.ratio$locus_name
-  data1.ratio$locus_name <- NULL
-  data1.ratio$class <- NULL
-  filter <- data.frame(row.names = row.names(data1.ratio))
-  filter$Nefref_1 <- rowSums(data1.ratio[, grepl("Nefref_1", colnames(data1.ratio))]) >= depth
-  filter$Refnef_1 <- rowSums(data1.ratio[, grepl("Refnef_1", colnames(data1.ratio))]) >= depth
-  filter$Nefref_2 <- rowSums(data1.ratio[, grepl("Nefref_2", colnames(data1.ratio))]) >= depth
-  filter$Refnef_2 <- rowSums(data1.ratio[, grepl("Refnef_2", colnames(data1.ratio))]) >= depth
-  data1.ratio$filter <- apply(filter, 1, any)
-  data.rm <- filter(data1.ratio, filter == TRUE)[, -ncol(data1.ratio)]
-  print(dim(data.rm))
-
-  data.rm2 <- data.rm
-
-  # matrate apply
-  data.rm2$mat_Refnef_1 <- data.rm$mat_Refnef_1 * data.rm$ratio
-  data.rm2$mat_Refnef_2 <- data.rm$mat_Refnef_2 * data.rm$ratio
-  data.rm2$mat_Nefref_1 <- data.rm$mat_Nefref_1 * data.rm$ratio
-  data.rm2$mat_Nefref_2 <- data.rm$mat_Nefref_2 * data.rm$ratio
-  mod_data1 <- data.rm2[, 1:8]
-  
-  return(mod_data1)
-}
-
-
-GLM_edgeR <- function(mod_data, cross, path){
+GLM_edgeR <- function(mod_data){
   # GLM fit
   edgeR <- DGEList(counts=mod_data, group=mother)
   edgeR <- calcNormFactors(edgeR)
@@ -68,191 +54,12 @@ GLM_edgeR <- function(mod_data, cross, path){
   edgeR <- estimateGLMTagwiseDisp(edgeR, edgeR.design)
   edgeR.fit <- glmFit(edgeR, edgeR.design)
   edgeR.lrt <- glmLRT(edgeR.fit, coef= "mothermother")
-
-  # BVC
-  tiff(paste0(path, "/BCV_", cross, ".tiff"), pointsize=30, width=param_fig[1], height=param_fig[2])
-  plotBCV(edgeR)
-  dev.off()
-  rm(edgeR)
-  rm(edgeR.fit)
-
-  # result table
-  test_result <- as.data.frame(edgeR.lrt$table)
   BH <- decideTestsDGE(edgeR.lrt, p=fdr_cutoff, adjust="BH")
-  BH_list <- cbind(test_result, BH)
-  BH_list$QValue <- p.adjust(BH_list$PValue, method="BH")
-  BH_list$locus_name <- row.names(BH_list)
-  BH_list <- BH_list[, c(7, 1:4,6,5)]
-  print(dim(BH_list))
-
-  BH_list <- merge(BH_list, anno[, c(1,7)], by = "locus_name")
-  print(dim(BH_list))
-
-  outfile <- paste0(path, "/List_edgR_", cross, ".txt")
-  write.table(BH_list, outfile, append=F, quote=F, row.names=F, sep ="\t")
-  rm(outfile)
-
-  # plot MA
-  outfig <- paste0(path, "/MA_", cross, ".tiff")
-  tiff(outfig, pointsize=30, width=param_fig[1], height=param_fig[2])
-  tag <- rownames(BH_list)[BH_list$PValue < fdr_cutoff]
-  plotSmear(edgeR.lrt, de.tags=tag, ylim = c(-15, 15))
-  dev.off()
-
-  rm(edgeR.lrt)
-  rm(test_result)
-  rm(tag)
-  rm(BH_list)
-  rm(outfig)
-
   return(BH)
 }
 
-MatRatePlot.filt <- function(data, data.filt, cross, BH.all, BH.filt, path, title){
 
-Count.Data <- data
-BH <- BH.all
-
-Count.Data.rate <- data.frame(Gene=row.names(Count.Data))
-Count.Data.rate$mat_RxN <- apply(Count.Data[, grepl("mat_Refnef",
-                                                    colnames(Count.Data))], 1, mean)
-Count.Data.rate$pat_RxN <- apply(Count.Data[, grepl("pat_Refnef",
-                                                    colnames(Count.Data))], 1, mean)
-Count.Data.rate$RxN_total <- with(Count.Data.rate, mat_RxN + pat_RxN)
-
-Count.Data.rate$mat_NxR <- apply(Count.Data[, grepl("mat_Nefref",
-                                                    colnames(Count.Data))], 1, mean)
-Count.Data.rate$pat_NxR <- apply(Count.Data[, grepl("pat_Nefref",
-                                                    colnames(Count.Data))], 1, mean)
-Count.Data.rate$NxR_total <- with(Count.Data.rate, mat_NxR + pat_NxR)
-
-Count.Data$pat_Refnef_1 <- Count.Data$pat_Refnef_1*2
-Count.Data$pat_Refnef_2 <- Count.Data$pat_Refnef_2*2
-Count.Data$pat_Nefref_1 <- Count.Data$pat_Nefref_1*2
-Count.Data$pat_Nefref_2 <- Count.Data$pat_Nefref_2*2
-
-Count.Data.rate$Refnef_1.rate <- with(Count.Data, mat_Refnef_1 / (mat_Refnef_1 + pat_Refnef_1))
-Count.Data.rate$Refnef_2.rate <- with(Count.Data, mat_Refnef_2  / (mat_Refnef_2 + pat_Refnef_2 ))
-Count.Data.rate$Nefref_1.rate <- with(Count.Data, mat_Nefref_1  / (pat_Nefref_1 + mat_Nefref_1 ))
-Count.Data.rate$Nefref_2.rate <- with(Count.Data, mat_Nefref_2  / (pat_Nefref_2 + mat_Nefref_2))
-Count.Data.rate$Refnef.rate <- with(Count.Data.rate, (Refnef_1.rate + Refnef_2.rate)/2)
-Count.Data.rate$Nefref.rate <- with(Count.Data.rate, (Nefref_1.rate + Nefref_2.rate)/2)
-
-# result table
-rate_list <- cbind(Count.Data.rate, BH)
-rate_list$locus_name <- row.names(rate_list)
-rate_list_meg <- filter(rate_list, mothermother == 1,
-                        Refnef_1.rate >= cutoff.mat, Refnef_2.rate >= cutoff.mat, Nefref_1.rate >= cutoff.mat, Nefref_2.rate >= cutoff.mat)
-rate_list_peg <- filter(rate_list, mothermother == -1,
-                        Refnef_1.rate <= cutoff.pat, Refnef_2.rate <= cutoff.pat, Nefref_1.rate <= cutoff.pat, Nefref_2.rate <= cutoff.pat)
-rate_list_Not <- filter(rate_list, !locus_name %in% c(rate_list_meg$locus_name, rate_list_peg$locus_name))
-rate_list_Not$mothermother <- 0
-rate_list <- rbind(rate_list_meg, rate_list_peg, rate_list_Not)
-
-head(rate_list)
-rm(Count.Data, Count.Data.rate)
-
-## MEGs
-Count.Data <- data.filt
-BH <- BH.filt
-
-#Count.Data <- mod_data1filt
-#BH <- BH1filt
-
-Count.Data.rate <- data.frame(Gene=row.names(Count.Data))
-Count.Data.rate$mat_RxN <- apply(Count.Data[, grepl("mat_Refnef", colnames(Count.Data))], 1, mean)
-Count.Data.rate$pat_RxN <- apply(Count.Data[, grepl("pat_Refnef", colnames(Count.Data))], 1, mean)
-Count.Data.rate$RxN_total <- with(Count.Data.rate, mat_RxN + pat_RxN)
-Count.Data.rate$mat_NxR <- apply(Count.Data[, grepl("mat_Nefref", colnames(Count.Data))], 1, mean)
-Count.Data.rate$pat_NxR <- apply(Count.Data[, grepl("pat_Nefref", colnames(Count.Data))], 1, mean)
-Count.Data.rate$NxR_total <- with(Count.Data.rate, mat_NxR + pat_NxR)
-
-Count.Data$pat_Refnef_1 <- Count.Data$pat_Refnef_1*2
-Count.Data$pat_Refnef_2 <- Count.Data$pat_Refnef_2*2
-Count.Data$pat_Nefref_1 <- Count.Data$pat_Nefref_1*2
-Count.Data$pat_Nefref_2 <- Count.Data$pat_Nefref_2*2
-
-Count.Data.rate$Refnef_1.rate <- with(Count.Data, mat_Refnef_1 / (mat_Refnef_1 + pat_Refnef_1))
-Count.Data.rate$Refnef_2.rate <- with(Count.Data, mat_Refnef_2  / (mat_Refnef_2 + pat_Refnef_2 ))
-Count.Data.rate$Nefref_1.rate <- with(Count.Data, mat_Nefref_1  / (pat_Nefref_1 + mat_Nefref_1 ))
-Count.Data.rate$Nefref_2.rate <- with(Count.Data, mat_Nefref_2  / (pat_Nefref_2 + mat_Nefref_2))
-Count.Data.rate$Refnef.rate <- with(Count.Data.rate, (Refnef_1.rate + Refnef_2.rate)/2)
-Count.Data.rate$Nefref.rate <- with(Count.Data.rate, (Nefref_1.rate + Nefref_2.rate)/2)
-
-head(Count.Data.rate)
-
-# Count.Data.rate$mean <- (Count.Data.rate$Refnef.rate + Count.Data.rate$Nefref.rate) / 2
-# mat_rate <- Count.Data.rate[c("Gene", "mean")]
-# names(mat_rate)[1] <- "locus_name"
-
-# result table
-rate_list.filt <- cbind(Count.Data.rate, BH)
-rate_list.filt$locus_name <- row.names(rate_list.filt)
-rate_list_meg.filt <- filter(rate_list.filt,
-                             mothermother == 1,
-                             Refnef_1.rate >= cutoff.mat,
-                             Refnef_2.rate >= cutoff.mat,
-                             Nefref_1.rate >= cutoff.mat,
-                             Nefref_2.rate >= cutoff.mat)
-
-rate_list2 <- merge(rate_list,
-                    rate_list_meg.filt[,c(14,15)], by = "locus_name", all= T)
-names(rate_list2)[15:16] <- c("mothermother","filt")
-
-# NA replace 0
-rate_list2$filt[is.na(rate_list2$filt)] <- 0
-
-# replace by SC filter
-rate_list2$mothermother[rate_list2$mothermother == 1 & rate_list2$filt == 0] <- 0
-rate_list2$mothermother[rate_list2$mothermother == 0 & rate_list2$filt == 1] <- 1
-
-outfile <- paste0(path, "/List_MatRate_", cross, ".txt")
-write.table(rate_list2, outfile, append=F, quote=F, row.names=F, sep ="\t")
-
-
-# plot
-rate_list_meg <- subset(rate_list2, mothermother == 1)
-rate_list_peg <- subset(rate_list2, mothermother == -1)
-rate_list_Not <- subset(rate_list2, mothermother == 0)
-
-rate_list_meg$class <- "MEG"
-rate_list_peg$class <- "PEG"
-rate_list_Not$class <- "Bi-allele"
-
-ggplot(data=rate_list_Not) +
-  geom_point(data=rate_list_Not,aes(x=Refnef.rate,
-                                    y=Nefref.rate,
-                                    color=class, alpha=class)) +
-  geom_point(data=rate_list_meg,aes(x=Refnef.rate,
-                                    y=Nefref.rate,
-                                    color=class)) +
-  geom_point(data=rate_list_peg,aes(x=Refnef.rate,
-                                    y=Nefref.rate,
-                                    color=class)) +
-  theme_bw() +
-  scale_color_manual(values=c("darkgrey","firebrick","steelblue")) +
-  theme(legend.position = 'none',
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_rect(fill = "white", colour = "black", size = 1) ,
-        axis.ticks=element_line(size = 1, colour = "black"),
-        plot.title = element_text(hjust = 0.5),
-        axis.text = element_text(face="plain", color="black",size=14, family = "Helvetica"),
-        axis.title=element_text(face="plain", color="black",size=14)) +
-  labs(title=title) +
-  xlab(paste0("Average maternal rate\n(", cv[1], " × ", cv[2], ")")) +
-  ylab(paste0("Average maternal rate\n(", cv[2], " × ", cv[1], ")"))
-
-ggsave(paste0(path, "/plot_", cross, ".eps"),
-       dpi = 1200, width = 5, height = 5.1,
-       device=cairo_ps)
-
-}
-
-
-MatRatePlot.New <- function(data, cross, BH, path){
-
-  Count.Data <- data
+findMEGs <- function(Count.Data, BH){
 
   Count.Data.rate <- data.frame(Gene=row.names(Count.Data))
   Count.Data.rate$mat_RxN <- apply(Count.Data[, grepl("mat_Refnef", colnames(Count.Data))], 1, mean)
@@ -275,50 +82,54 @@ MatRatePlot.New <- function(data, cross, BH, path){
   Count.Data.rate$Nefref.rate <- with(Count.Data.rate, (Nefref_1.rate + Nefref_2.rate)/2)
 
   # result table
+  rate_list.filt <- cbind(Count.Data.rate, BH)
+  rate_list.filt$locus_name <- row.names(rate_list.filt)
+  rate_list_meg <- filter(rate_list.filt,
+                               mothermother == 1,
+                               Refnef_1.rate >= cutoff.mat,
+                               Refnef_2.rate >= cutoff.mat,
+                               Nefref_1.rate >= cutoff.mat,
+                               Nefref_2.rate >= cutoff.mat)
+
+  return(rate_list_meg)
+
+}
+
+findPEGs <- function(Count.Data, BH){
+
+  Count.Data.rate <- data.frame(Gene=row.names(Count.Data))
+  Count.Data.rate$mat_RxN <- apply(Count.Data[, grepl("mat_Refnef",
+                                                      colnames(Count.Data))], 1, mean)
+  Count.Data.rate$pat_RxN <- apply(Count.Data[, grepl("pat_Refnef",
+                                                      colnames(Count.Data))], 1, mean)
+  Count.Data.rate$RxN_total <- with(Count.Data.rate, mat_RxN + pat_RxN)
+
+  Count.Data.rate$mat_NxR <- apply(Count.Data[, grepl("mat_Nefref",
+                                                      colnames(Count.Data))], 1, mean)
+  Count.Data.rate$pat_NxR <- apply(Count.Data[, grepl("pat_Nefref",
+                                                      colnames(Count.Data))], 1, mean)
+  Count.Data.rate$NxR_total <- with(Count.Data.rate, mat_NxR + pat_NxR)
+
+  Count.Data$pat_Refnef_1 <- Count.Data$pat_Refnef_1*2
+  Count.Data$pat_Refnef_2 <- Count.Data$pat_Refnef_2*2
+  Count.Data$pat_Nefref_1 <- Count.Data$pat_Nefref_1*2
+  Count.Data$pat_Nefref_2 <- Count.Data$pat_Nefref_2*2
+
+  Count.Data.rate$Refnef_1.rate <- with(Count.Data, mat_Refnef_1 / (mat_Refnef_1 + pat_Refnef_1))
+  Count.Data.rate$Refnef_2.rate <- with(Count.Data, mat_Refnef_2  / (mat_Refnef_2 + pat_Refnef_2 ))
+  Count.Data.rate$Nefref_1.rate <- with(Count.Data, mat_Nefref_1  / (pat_Nefref_1 + mat_Nefref_1 ))
+  Count.Data.rate$Nefref_2.rate <- with(Count.Data, mat_Nefref_2  / (pat_Nefref_2 + mat_Nefref_2))
+
+  # result table
   rate_list <- cbind(Count.Data.rate, BH)
-
   rate_list$locus_name <- row.names(rate_list)
-  rate_list_meg <- filter(rate_list,
-                          mothermother == 1,
-                          Refnef_1.rate >= cutoff.mat, Refnef_2.rate >= cutoff.mat, Nefref_1.rate >= cutoff.mat, Nefref_2.rate >= cutoff.mat)
-  rate_list_peg <- filter(rate_list,
-                          mothermother == -1,
-                          Refnef_1.rate <= cutoff.pat, Refnef_2.rate <= cutoff.pat, Nefref_1.rate <= cutoff.pat, Nefref_2.rate <= cutoff.pat)
-  rate_list_Not <- filter(rate_list, !locus_name %in% c(rate_list_meg$locus_name, rate_list_peg$locus_name))
-  rate_list_Not$mothermother <- 0
-  rate_list <- rbind(rate_list_meg, rate_list_peg, rate_list_Not)
 
-  outfile <- paste0(path, "/List_MatRate_", cross, ".txt")
-  write.table(rate_list, outfile, append=F, quote=F, row.names=F, sep ="\t")
+  rate_list_peg <- filter(rate_list, mothermother == -1,
+                          Refnef_1.rate <= cutoff.pat,
+                          Refnef_2.rate <= cutoff.pat,
+                          Nefref_1.rate <= cutoff.pat,
+                          Nefref_2.rate <= cutoff.pat)
 
+  return(rate_list_peg)
 
-  rate_list_meg$class <- "MEG"
-  rate_list_peg$class <- "PEG"
-  rate_list_Not$class <- "Bi-allele"
-
-  ggplot(data=rate_list_Not) +
-    geom_point(data=rate_list_Not,aes(x=Refnef.rate,
-                                      y=Nefref.rate,
-                                      color=class, alpha=class)) +
-    geom_point(data=rate_list_meg,aes(x=Refnef.rate,
-                                      y=Nefref.rate,
-                                      color=class)) +
-    geom_point(data=rate_list_peg,aes(x=Refnef.rate,
-                                      y=Nefref.rate,
-                                      color=class)) +
-    theme_bw() +
-    scale_color_manual(values=c("darkgrey","firebrick","steelblue")) +
-    theme(legend.position = 'none',
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.background = element_rect(fill = "white", colour = "black", size = 1) ,
-          axis.ticks=element_line(size = 1, colour = "black"),
-          plot.title = element_text(hjust = 0.5),
-          axis.text = element_text(color="black", size=12),
-          axis.title=element_text(size=14,face="bold")) +
-    xlab(paste0("Maternal rate in ", cv[1], " x ", cv[2])) +
-    ylab(paste0("Maternal proportion in ", cv[2], " x ", cv[1]))
-
-  ggsave(paste0(path, "/plot_", cross, ".tiff"),
-         dpi = 300, width = 5.4, height = 4.8)
 }
--- 02.identify_impriomnted_genes.R
+++ 02.identify_impriomnted_genes.R
@@ -1,51 +1,41 @@
 source ("01.function.R")
 
-# ref: reference allele, nef: non-reference allele
-options(width = 120)
-fdr_cutoff <- 0.05          # FDR cut-off 5%
+fdr_cutoff <- 0.05
 cutoff.mat <- 0.70
 cutoff.pat <- 0.30
 depth <- 20
 cv <- c("NIP", "KAS")
-head <- c("locus_name","mat_Refnef_1","pat_Refnef_1","mat_Refnef_2","pat_Refnef_2","pat_Nefref_1","mat_Nefref_1","pat_Nefref_2","mat_Nefref_2")
-
-
+head <- c("locus_name",
+          "mat_Refnef_1","pat_Refnef_1","mat_Refnef_2","pat_Refnef_2",
+          "pat_Nefref_1","mat_Nefref_1","pat_Nefref_2","mat_Nefref_2")
 
 ######################
 # load and prepare data
 ######################
 # ref
-seed <- read.table("./R/list/seed_cort_gene.ratio.txt", header = T)
+seed <- read.table("seed_cort_gene.ratio.txt", header = T)
 
-# import count data
-RefNef1 <- read.table("NK_count_r1.txt", header = T)[, c(-2:-6)]
-RefNef2 <- read.table("NK_count_r2.txt", header = T)[, c(-2:-6)]
-NefRef1 <- read.table("KN_count_r1.txt", header = T)[, c(-2:-6)]
-NefRef1 <- read.table("KN_count_r2.txt", header = T)[, c(-2:-6)]
+# import read count data from featureCounts
+RefNef <- read.table("NipKas_count.txt", header = T)[, c(-2:-6)]
+NefRef <- read.table("KasNip_count.txt", header = T)[, c(-2:-6)]
 
-data1 <- cbind(RefNef1[, c(1, 2:5)], NefRef1[, c(2:5)])      # 3DAP
-data2 <- cbind(RefNef1[, c(1, 6:9)], NefRef1[, c(6:9)])      # 5DAP
-data3 <- cbind(RefNef2[, c(1, 2:5)], RefNef2[, c(6:9)])      # 7DAP
-data4 <- cbind(RefNef1[, c(1, 14:17)], NefRef1[, c(14:17)])  # embryo
+data1 <- cbind(RefNef[, c(1, 2:5)], NefRef[, c(2:5)])      # 3DAP
+data2 <- cbind(RefNef[, c(1, 6:9)], NefRef[, c(6:9)])      # 5DAP
+data3 <- cbind(RefNef[, c(1, 10:13)], NefRef[, c(10:13)])  # 7DAP
+data4 <- cbind(RefNef[, c(1, 14:17)], NefRef[, c(14:17)])  # embryo
 
 # Assign maternal and paternal depending on cross direction
-mod_data_paternal <- modidyData(data1, depth)
-mod_data_maternal <- modidyData.SCfilt(data1, depth, seed)
+mod_data_maternal <- modidyData.mat(data1, depth, seed)
+mod_data_paternal <- modidyData.pat(data1, depth)
 
-######################
-# create design matrix
-#####################
+# edgeR
 cros = factor(c(rep("Refnef", 3), rep("Nefref", 3)))
 mother = factor(rep(c("mother","father"), 3))
 edgeR.design <- model.matrix( ~ cros + mother)
 
-######################
-# detect allele-specific reads
-#####################
-BH1 <- GLM_edgeR(mod_data1, "NK_3DAP", out_path)
-BH1filt <- GLM_edgeR(mod_data1filt, "NK_3DAP", out_path)
+BH_maternal <- GLM_edgeR(mod_data_maternal)
+BH_paternal <- GLM_edgeR(mod_data_paternal)
 
-######################
-# parental proportion by TMM normalized
-#####################
-MatRatePlot.filt(mod_data1, mod_data1filt, "NK_3DAP.filt", BH1, BH1filt, out_path, "NIP-KAS 3 DAP endosperm")
+# filt by maternal rate
+meg_lis <- findMEGs(mod_data1, BH1)
+peg_lis <- findPEGs(mod_data1, BH1filt)
