d241f5a3f49abe19a8f00a59df450e95d0274519
ktonosaki <67316927+ktonosaki@users.noreply.github.com>
2024-10-03 10:56:17 +0900

    Update 02.identify_impriomnted_genes.R

--- 02.identify_impriomnted_genes.R
+++ 02.identify_impriomnted_genes.R
@@ -37,5 +37,5 @@ BH_maternal <- GLM_edgeR(mod_data_maternal)
 BH_paternal <- GLM_edgeR(mod_data_paternal)
 
 # filt by maternal rate
-meg_lis <- findMEGs(mod_data1, BH_maternal)
-peg_lis <- findPEGs(mod_data1, BH_paternal)
+meg_lis <- findMEGs(mod_data_maternal, BH_maternal)
+peg_lis <- findPEGs(mod_data_paternal, BH_paternal)
